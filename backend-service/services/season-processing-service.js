060123677ed7a59546fe08a4711c8ac1da5f7c4e
Seth Theeke <seththeeke@gmail.com>
2020-12-26 15:35:30 -0500

    add boilerplate season processing outline

---
+++ backend-service/services/season-processing-service.js
@@ -0,0 +1,19 @@
+var badgesDao = require('./../dao/badges-dao');
+var leagueDetailsDao = require('./../dao/league-details-dao');
+var fplDraftService = require('./fpl-draft-service');
+
+module.exports = {
+    extractSeasonData: async function(extractSeasonDataRequest){
+        // Bootstrap Static
+        // https://draft.premierleague.com/api/bootstrap-static - Can also be fetched from S3 based
+        // League Details
+        // https://draft.premierleague.com/api/league/11133/details
+        // Top Elements
+        // https://draft.premierleague.com/api/top-elements
+        // All transactions
+        // https://draft.premierleague.com/api/draft/league/11133/transactions
+        // Fixture and Player history for season given player id
+        // https://draft.premierleague.com/api/element-summary/254
+        return extractSeasonDataRequest;
+    }
+}
