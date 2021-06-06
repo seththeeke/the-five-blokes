6ccce95699e2405adfe6be2bbf66e014fa328d66
Seth Theeke <seththeeke@gmail.com>
2021-06-05 19:08:48 -0500

    update color scheme based on sample five blokes image

--- mobile-web-app/src/components/AppHeader.js
+++ mobile-web-app/src/components/AppHeader.js
@@ -3,13 +3,15 @@ import {
   Link
 } from "react-router-dom";
 import './../css/AppHeader.css';
+import fiveBlokesLogo from './../img/five-blokes-sample.png';
 
 class AppHeader extends React.Component {
     render() {
         return (
             <div className="header-wrapper">
                 <Link to="/home">
-                    <div className="header">The Five Blokes</div>
+                    {/* <div className="header">The Five Blokes</div> */}
+                    <img className="header-logo" src={fiveBlokesLogo} alt="five-blokes-logo"></img>
                 </Link>
             </div>
         );
