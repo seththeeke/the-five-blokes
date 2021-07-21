877038fa23625b61da6c00e14e78f01b8a118c42
Seth Theeke <seththeeke@gmail.com>
2021-07-20 20:57:36 -0500

    add links to merch

--- mobile-web-app/src/components/pages/Podcast.js
+++ mobile-web-app/src/components/pages/Podcast.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './../../css/Podcast.css';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { TwitterTimelineEmbed } from 'react-twitter-embed';
-import theFiveBlokes from './../../img/blokes/Blokes-group-small.jpg';
+import hoodie from './../../img/merch/hoodie.jpeg';
+import hat from './../../img/merch/hat.jpg';
+import sticker from './../../img/merch/sticker.jpg';
 
 class Podcast extends React.Component {
    constructor(props){
@@ -39,6 +41,18 @@ class Podcast extends React.Component {
                   {/* <img className="the-five-blokes-heads" src={theFiveBlokes} alt="five-blokes-logo"></img> */}
                   <div className="grid-item">
                      <iframe title="spotify" src="https://open.spotify.com/embed-podcast/show/7Hw0EJXJE2Zmi4pZ13aehl" width="100%" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                  </div>
+                  <div>
+                     <div className="shop-blokes-title">Shop Merch!</div>
+                     <a href="https://www.etsy.com/shop/TheFiveBlokes?ref=search_shop_redirect">
+                        <img className="merch" src={hoodie} alt="hoodie"></img>
+                     </a>
+                     <a href="https://www.etsy.com/shop/TheFiveBlokes?ref=search_shop_redirect">
+                        <img className="merch" src={sticker} alt="hoodie"></img>
+                     </a>
+                     <a href="https://www.etsy.com/shop/TheFiveBlokes?ref=search_shop_redirect">
+                        <img className="merch" src={hat} alt="hoodie"></img>
+                     </a>
                   </div>
                   {/* <div className="grid-item">
                      <div className="twitter-embed-wrapper">
